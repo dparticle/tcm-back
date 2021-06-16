06ae5af6bcde9add7ea98db5f6f9f5e87535fd13
dparticle <dparticle@163.com>
2021-06-16 10:51:19 +0800

    chore: 热门文章假数据 & api 修改

--- app/controller/tcm.js
+++ app/controller/tcm.js
@@ -67,8 +67,8 @@ class TcmController extends Controller {
     ctx.body = result[0];
   }
 
-  async getDailyRecommend() {
-    console.log('GET /tcm/recommend');
+  async getRecommendTcm() {
+    console.log('GET /recommend/tcm');
     const { ctx } = this;
     // 获取当天推荐的 tcm 的 id
     const recommendIdList = await this.app.mysql.query('SELECT tcm_id FROM recommend WHERE TO_DAYS(create_time) = TO_DAYS(NOW())');
@@ -77,6 +77,38 @@ class TcmController extends Controller {
       result.push(await this.getInfoById(item.tcm_id, [ 'id', 'name', 'actions' ]));
     }
     ctx.body = result;
+  }
+
+  async getRecommendArticle() {
+    console.log('GET /recommend/article');
+    const { ctx } = this;
+    ctx.body = [
+      {
+        title: '孙达出席中医药—尤纳尼传统医药国际研讨会暨中国—巴基斯坦中医药中心揭牌仪式',
+        url: 'http://ghs.satcm.gov.cn/gongzuodongtai/2021-06-10/21999.html',
+        date: '2021-06-10',
+      },
+      {
+        title: '孙达出席中医药—尤纳尼传统医药国际研讨会暨中国—巴基斯坦中医药中心揭牌仪式',
+        url: 'http://ghs.satcm.gov.cn/gongzuodongtai/2021-06-10/21999.html',
+        date: '2021-06-10',
+      },
+      {
+        title: '孙达出席中医药—尤纳尼传统医药国际研讨会暨中国—巴基斯坦中医药中心揭牌仪式',
+        url: 'http://ghs.satcm.gov.cn/gongzuodongtai/2021-06-10/21999.html',
+        date: '2021-06-10',
+      },
+      {
+        title: '孙达出席中医药—尤纳尼传统医药国际研讨会暨中国—巴基斯坦中医药中心揭牌仪式',
+        url: 'http://ghs.satcm.gov.cn/gongzuodongtai/2021-06-10/21999.html',
+        date: '2021-06-10',
+      },
+      {
+        title: '孙达出席中医药—尤纳尼传统医药国际研讨会暨中国—巴基斯坦中医药中心揭牌仪式',
+        url: 'http://ghs.satcm.gov.cn/gongzuodongtai/2021-06-10/21999.html',
+        date: '2021-06-10',
+      },
+    ];
   }
 }
 
--- app/router.js
+++ app/router.js
@@ -10,7 +10,8 @@ module.exports = app => {
   router.post('/tcm/rough', controller.tcm.searchAllRoughInfo);
   router.post('/tcm', app.jwt, controller.tcm.searchCompleteInfoById);
   router.get('/tcm/sum', controller.tcm.getSum);
-  router.get('/tcm/recommend', controller.tcm.getDailyRecommend);
+  router.get('/recommend/tcm', controller.tcm.getRecommendTcm);
+  router.get('/recommend/article', controller.tcm.getRecommendArticle);
   // user api
   router.post('/user/login', controller.user.login);
   router.post('/user/reg', controller.user.reg);
